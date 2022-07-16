d0d4ff95fa4f646e13f9a6f82305f0f57008ede2
Dayana Horna <dayisitax@gmail.com>
2022-07-16 11:35:42 -0500

    JS Channel dinamico y navegable en ambos menus, vertical y horinzontal

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,25 @@
 // lógica central de la aplicación
+let channels = document.querySelectorAll(".channel");
+
+channels.forEach((element) => {
+    element.addEventListener("click", function (e){
+        const previos = document.querySelectorAll(".channel.-active")
+        previos.forEach((element) => {
+          element.classList.remove("-active");
+        });
+
+    const nameChannel = e.currentTarget.dataset.name;
+    let channelsSelected = document.querySelectorAll(`[data-name="${nameChannel}"]`);
+    channelsSelected.forEach((element) => {
+      element.classList.add("-active")
+
+        console.log(element)
+    })
+        
+    })
+})
+
+
+let generals = document.querySelectorAll('[data-name="general"]');
+let forobardos = document.querySelectorAll('[data-name="forobardo"]');
+let randoms = document.querySelectorAll('[data-name="random"]');
