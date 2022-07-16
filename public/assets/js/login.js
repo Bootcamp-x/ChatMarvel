a6f3aee357428cf10afcd838fc9cf3fc67e469ce
Ronaldoa1698 <ronaldo.delgado@unas.edu.pe>
2022-07-16 10:36:52 -0500

    capturar username value

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,1 +1,17 @@
 // lógica para el login
+
+let $username = document.getElementById("js-input-username");
+let buttonEnter = document.getElementById("js-button-loggin");
+
+//checkUser();
+
+buttonEnter.addEventListener("click", function (event) {
+  event.preventDefault();
+  let usernameValue = $username.value;
+  if (usernameValue) {
+    console.log(usernameValue);
+    //guardar username en local storage
+  } else {
+    console.log("vacio");
+  }
+});
